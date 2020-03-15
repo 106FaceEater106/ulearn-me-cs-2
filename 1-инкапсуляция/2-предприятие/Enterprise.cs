fea2d29bfb0e25723c9bfc92742efd667cccd768
Timofei <tuistssip@gmail.com>
2020-03-16 02:22:10 +0500

    +1.2

---
+++ 1-инкапсуляция/2-предприятие/Enterprise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Incapsulation.EnterpriseTask
+{
+  public class Enterprise
+  {
+    Guid guid;
+    public readonly Guid Guid;
+
+    public Enterprise(Guid guid) => this.guid = guid;
+
+    public string Name { get; set; }
+
+    string inn;
+    public string Inn
+    {
+      get => inn;
+      set {
+        if (value.Length != 10 || !value.All(z => char.IsDigit(z)))
+          throw new ArgumentException();
+        inn = value;
+      }
+    }
+
+    public DateTime EstablishDate { get; set; }
+
+    public TimeSpan ActiveTimeSpan => DateTime.Now - EstablishDate;
+
+    public double GetTotalTransactionsAmount()
+    {
+      DataBase.OpenConnection();
+      var amount = 0.0;
+      foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == guid))
+        amount += t.Amount;
+      return amount;
+    }
+  }
+}
---
+++ 1-инкапсуляция/2-предприятие/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Incapsulation.EnterpriseTask
+{
+  public class Transaction
+  {
+    public double Amount;
+    public Guid EnterpriseGuid;
+  }
+}
