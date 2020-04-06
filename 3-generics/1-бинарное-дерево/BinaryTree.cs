ed04fba133516473d23438c1a61becc5b41cbf8f
Timofei <tuistssip@gmail.com>
2020-04-06 17:43:55 +0500

    '+3.1'

---
+++ 3-generics/1-бинарное-дерево/BinaryTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics.BinaryTrees
+{
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private bool initialized = false;
+
+        public BinaryTree<T> Left { get; private set; }
+        public BinaryTree<T> Right { get; private set; }
+        public T Value { get; private set; }
+
+        public void Add(T newValue)
+        {
+            if (!initialized)
+            {
+                Value = newValue;
+                initialized = true;
+            }
+            else if (newValue.CompareTo(Value) < 1)
+            {
+                if (Left == null) Left = new BinaryTree<T>();
+                Left.Add(newValue);
+            }
+            else
+            {
+                if (Right == null) Right = new BinaryTree<T>();
+                Right.Add(newValue);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (Left != null)
+                foreach (var n in Left)
+                    yield return n;
+            if (initialized)
+                yield return Value;
+            if (Right != null)
+                foreach (var n in Right)
+                    yield return n;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public static class BinaryTree
+    {
+        public static BinaryTree<T> Create<T>(params T[] items) where T : IComparable<T>
+        {
+            BinaryTree<T> tree = new BinaryTree<T>();
+            foreach (var item in items)
+                tree.Add(item);
+            return tree;
+        }
+    }
+}
